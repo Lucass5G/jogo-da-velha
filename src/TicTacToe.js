8fe50a7f7e254dffbd4fb2945dc8472cee68a056
Lucass5g <desenvolverbg@gmail.com>
2021-02-22 21:24:37 -0300

    Primeira versão do Jogo da Velha

---
+++ src/TicTacToe.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import "./TicTacToe.css";
+
+function TicTacToe() {
+  const emptyBoard = Array(9).fill("");
+  const [board, setBoard] = useState(emptyBoard);
+
+  const [currentPlayer, setCurrentPlayer] = useState("O");
+
+  const [winner, setWinner] = useState(null);
+
+  const handleCellClick = (index) => {
+    //Verifica se o jogo acabou
+    if (winner) {
+      console.log("vai porra");
+      return null;
+    }
+
+    //Verifica se já foi marcado X ou O
+    if (board[index] !== "") return null;
+
+    setBoard(
+      board.map((item, itemIndex) =>
+        itemIndex === index ? currentPlayer : item
+      )
+    );
+
+    //Alternar entre X e O
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  };
+
+  const checkWinner = () => {
+    //Constante com todas possibilidades de ganhar
+    const possibleWaysToWin = [
+      [board[0], board[1], board[2]],
+      [board[3], board[4], board[5]],
+      [board[6], board[7], board[8]],
+
+      [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]],
+      [board[2], board[5], board[8]],
+
+      [board[0], board[4], board[8]],
+      [board[2], board[4], board[6]],
+    ];
+
+    //Verificar quem ganhou
+    possibleWaysToWin.forEach((cells) => {
+      if (cells.every((cell) => cell === "O")) setWinner("O");
+      if (cells.every((cell) => cell === "X")) setWinner("X");
+    });
+
+    checkDraw();
+  };
+
+  //Empatou
+  const checkDraw = () => {
+    if (board.every((item) => item !== "")) setWinner("D");
+  };
+
+  useEffect(checkWinner, [board]);
+
+  const resetGame = () => {
+    setCurrentPlayer("O");
+    setBoard(emptyBoard);
+    setWinner(null);
+  };
+
+  return (
+    <main>
+      <h1 className="title">Jogo da Velha</h1>
+
+      <div className={`board ${winner ? "game-over" : ""}`}>
+        {board.map((item, index) => (
+          <div
+            key={index}
+            className={`cell ${item}`}
+            onClick={() => handleCellClick(index)}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+
+      {winner && (
+        <footer>
+          {winner === "D" ? 
+            <h2 className="winner-message">
+              <span className={winner}>Empate!</span>
+            </h2>
+           : 
+            <h2 className="winner-message">
+              <span className={winner}>{winner}</span> Venceu!
+            </h2>
+          }
+          <button onClick={resetGame}>Recomeçar</button>
+        </footer>
+      )}
+    </main>
+  );
+}
+
+export default TicTacToe;
